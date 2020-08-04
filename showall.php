6dc919c162b668916bbc759105b7990228d9b2e1
Kahlil <51727305+grocottk@users.noreply.github.com>
2020-08-05 10:55:20 +1200

    Showall Page after "Results Part 1" Video
    This is the build of this project that has updated the "showall" page with the ability to show results.

--- showall.php
+++ showall.php
@@ -1,14 +1,63 @@
-<?php include("topbit.php") ?>
+<?php include("topbit.php");
+
+    $find_sql = "SELECT *
+FROM `L2_91892_game_practice`
+LIMIT 0 , 30";
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+
+?>
 
         <div class="box main">
             <h2>All Results from Database</h2>
             
             
-            <p>
+            <?php
+            
+            if($count < 1) {
                 
-                [Results will appear here]
+            ?>
+            
+            <div class="error">
+            
+                Sorry, there are no results that match your search.
                 
-            </p>
+                Please use the search box in the side bar to try again.
+                
+            </div> <!-- end error -->
+            
+            <?php
+                
+            } // end no results if
+            
+            else {
+                
+                do
+                
+                {
+                    
+                    ?>
+            
+            <!-- Results go here -->
+            
+            <div class="results">
+            
+                You Have Results
+                
+            </div>
+            
+            <?php
+                    
+                } // end results 'do'
+                
+                while
+                    
+                    ($find_rs=mysqli_fetch_assoc($find_query));
+                
+            } // end else
+            
+            ?>
             
 
             

ab415c1102d54d3f79acc2319ae2cab5a0491f58
Kahlil <51727305+grocottk@users.noreply.github.com>
2020-08-20 22:03:52 +1200

    Practice Database Project after "Search Planning" Video
    This build of the Practice Databases Project shows a version that has refined the Acknowledgements that are given within the website itself. This version creates a more detailed, and organised approach to 'crediting and acknowledging' those relevant parties that assisted in the creation of this project.

--- index.php
+++ index.php
@@ -4,9 +4,17 @@
             <h2>Welcome</h2>
             
             <p>
+            
+                This database incldes data regarding roughly 200 of the most popular mobile strategy games on the Apple Appstore (as of 2019).
+            
+            </p>
+            
+            <h2>Credit and Acknowledgements:</h2>
+            
+            <p>
                 
-                This database incldes data regarding roughly 200 of the most popular mobile strategy games on the Apple Appstore (as of 2019). Thehref original dataset can be found <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">here</a>.
-                
+                The original dataset that this database uses can be found <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">here</a>.
+
             </p>
             
             <p>

037c1d4169fc52adf46f2ff3e8c0049b0984f6d2
Kahlil <51727305+grocottk@users.noreply.github.com>
2020-09-11 10:59:59 +1200

    During "Entry Form Setup Part 2" Video
    This build of the Practice Databases Project implements the first elements of the 'Add an Entry' page of the Database's Website.

--- addentry.php
+++ addentry.php
@@ -5,14 +5,22 @@
 $app_name = "";
 $subtitle = "";
 $url = "";
+$genreID = "";
 $dev_name = "";
 $age = "";
 $rating = "";
 $rate_count = "";
 $cost = "";
+$inapp = 1;
 $description = "";
 
 $has_errors = "no";
+
+// 'Code below executes when the form is submitted...
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "You pushed the button";
+} // 'End of Button Submitted code'
 
 ?>
 
@@ -23,7 +31,39 @@ $has_errors = "no";
 <h2>Add an Entry to this Database</h2>
                 
                 <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                    
+                <!-- 'App Name (Required)' -->
+                    
+                <input class="add-field" type="text" name="app_name" value="<?php echo $app_name; ?>" required placeholder="App Name and/or Title (Required)..."/>
+                    
+                <!-- 'Subtitle (Optional)' -->
+                    
+                <!-- 'URL (Required, Must start [with] http://)' -->
+                    
+                <!-- 'Genre Dropdown (Required)' -->
                 
+                <!-- 'Develper Name' (Required)' -->
+                    
+                <!-- 'Age (Set to 0 if [text box is] left blank)' -->
+                    
+                <!-- 'Rating (Number between 0 - 5, 1 dp)' -->
+                    
+                <!-- '[Number] of Ratings (Integer more than 0) -->
+                    
+                <!-- 'Cost (Decimal 2dp, must be more than 0)' -->
+                    
+                <!-- 'In-App Purchase radio buttons' -->
+                    
+                <!-- 'Description text area' -->
+                    
+                <!-- 'Submit Button' -->
+                
+                <p>
+                    
+                    <input class="submit advanced-button" type="submit" value="Submit" />
+                    
+                </p>
+                    
                 </form>
                 
             </div> <!-- End of 'Add Entry' 'div' -->
